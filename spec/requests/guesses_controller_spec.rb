bf183a1fa440bb291da2e2094a6a45f44b1f1d59
Keisuke Tanaka <keinskyrocket@KT.fritz.box>
2022-10-20 10:59:21 +1300

    Refactor the tests

--- spec/requests/guesses_controller_spec.rb
+++ spec/requests/guesses_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe GuessesController do
+  let (:game) { Game.new(
+    id: 5,
+    player_name: 'OW',
+    game_result: 'In progress',
+    secret_word: 'TURTLE'
+  ) }
+
   describe '#create' do
     context 'when the same guess was made' do
-      it 'should display a message that the guess is dupe' do
-        game = Game.create(
-          id: 5,
-          player_name: 'OW',
-          game_result: 'In progress',
-          secret_word: 'TURTLE'
-        )
-        
-        guessed = [
-          Guess.create(value: 'T', game: game), # Correct letter
-          Guess.create(value: 'A', game: game),
-        ]
+      before do
+        Guess.create(value: 'T', game: game) # Correct letter
+        Guess.create(value: 'A', game: game)
+      end
 
+      it 'should display a message that the guess is dupe' do
         expect(game.reload.game_result).to eq 'In progress'
         post game_guesses_path(game), params: { guess: { value: 'A' } }
         expect(response).to redirect_to game
@@ -25,26 +25,19 @@ describe GuessesController do
     end
 
     context 'when the guess loses the game' do
-      it 'should display a message that game is over' do
-        game = Game.create(
-          id: 5,
-          player_name: 'OW',
-          game_result: 'In progress',
-          secret_word: 'TURTLE'
-        )
-        
-        guessed = [
-          Guess.create(value: 'T', game: game), # Correct letter
-          Guess.create(value: 'A', game: game),
-          Guess.create(value: 'S', game: game),
-          Guess.create(value: 'D', game: game),
-          Guess.create(value: 'F', game: game),
-          Guess.create(value: 'G', game: game),
-          Guess.create(value: 'H', game: game),
-          Guess.create(value: 'J', game: game),
-          Guess.create(value: 'K', game: game)
-        ]
+      before do
+        Guess.create(value: 'T', game: game) # Correct letter
+        Guess.create(value: 'A', game: game)
+        Guess.create(value: 'S', game: game)
+        Guess.create(value: 'D', game: game)
+        Guess.create(value: 'F', game: game)
+        Guess.create(value: 'G', game: game)
+        Guess.create(value: 'H', game: game)
+        Guess.create(value: 'J', game: game)
+        Guess.create(value: 'K', game: game)
+      end
 
+      it 'should display a message that game is over' do
         expect(game.reload.game_result).to eq 'In progress'
         post game_guesses_path(game), params: { guess: { value: 'Z' } }
         expect(response).to redirect_to game
@@ -54,25 +47,6 @@ describe GuessesController do
       end
 
       it 'should set game_result to be Lose' do
-        game = Game.create(
-          id: 5,
-          player_name: 'OW',
-          game_result: 'In progress',
-          secret_word: 'TURTLE'
-        )
-        
-        guessed = [
-          Guess.create(value: 'T', game: game), # Correct letter
-          Guess.create(value: 'A', game: game),
-          Guess.create(value: 'S', game: game),
-          Guess.create(value: 'D', game: game),
-          Guess.create(value: 'F', game: game),
-          Guess.create(value: 'G', game: game),
-          Guess.create(value: 'H', game: game),
-          Guess.create(value: 'J', game: game),
-          Guess.create(value: 'K', game: game)
-        ]
-
         expect(game.reload.game_result).to eq 'In progress'
         post game_guesses_path(game), params: { guess: { value: 'Z' } }
         expect(response).to redirect_to game
@@ -83,24 +57,17 @@ describe GuessesController do
     end
 
     context 'when game is win' do
-      it 'should set game_result to be Win' do
-        game = Game.create(
-          id: 11,
-          player_name: 'OW',
-          game_result: 'In progress',
-          secret_word: 'TURTLE'
-        )
-        
-        guessed = [
-          Guess.create(value: 'T', game: game), # Correct letter
-          Guess.create(value: 'U', game: game), # Correct letter
-          Guess.create(value: 'R', game: game), # Correct letter
-          Guess.create(value: 'L', game: game), # Correct letter
-          Guess.create(value: 'F', game: game),
-          Guess.create(value: 'G', game: game),
-          Guess.create(value: 'H', game: game)
-        ]
+      before do
+        Guess.create(value: 'T', game: game) # Correct letter
+        Guess.create(value: 'U', game: game) # Correct letter
+        Guess.create(value: 'R', game: game) # Correct letter
+        Guess.create(value: 'L', game: game) # Correct letter
+        Guess.create(value: 'F', game: game)
+        Guess.create(value: 'G', game: game)
+        Guess.create(value: 'H', game: game)
+      end
 
+      it 'should set game_result to be Win' do
         expect(game.reload.game_result).to eq 'In progress'
         post game_guesses_path(game), params: { guess: { value: 'E' } }
         expect(response).to redirect_to game
